6c763035c41d2ab80e082db57900f96b178c9a6c
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2024-01-28 15:08:09 -0500

    reusable open ai, initial chatbot CLI

---
+++ chat.js
@@ -0,0 +1,48 @@
+import { openai } from './openai.js'
+import readline from 'node:readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const newMessage = async (history, message) => {
+  const results = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [...history, message],
+  })
+
+  return results.choices[0].message
+}
+
+const formatMessage = (userInput) => ({ role: 'user', content: userInput })
+
+const chat = () => {
+  const history = [
+    {
+      role: 'system',
+      content: 'You are an AI assistant.  Answer questions or else!',
+    },
+  ]
+
+  const start = () => {
+    rl.question('You: ', async (userInput) => {
+      if (userInput.toLowerCase() === 'exit') {
+        rl.close()
+        return
+      }
+
+      const message = formatMessage(userInput)
+      const response = await newMessage(history, message)
+
+      history.push(message, response)
+      console.log(`\n\nAI: ${response.content}:\n\n`)
+      start()
+    })
+  }
+  console.log('\n\nAI: How can I help you today?\n\n')
+  start()
+}
+
+console.log('Chatbot initialized.  Type "exit" to end the chat.')
+chat()
